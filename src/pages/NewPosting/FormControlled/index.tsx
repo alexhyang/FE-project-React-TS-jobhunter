14edb8250af9844a7c681a9903fb963dd47d6ccf
Alex Yang <alex.hyang.dev@gmail.com>
2023-07-28 15:44:55 -0700

    add alert to form submission

--- src/pages/NewPosting/FormControlled/index.tsx
+++ src/pages/NewPosting/FormControlled/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useForm, SubmitHandler } from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,73 +6,78 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { IPostingPost, IFormData } from "interfaces";
 import { baseURL } from "env";
 import { TYPE_OPTIONS, LEVEL_OPTIONS } from "../formSelectOptions";
+import { normalizeFormData } from "../helperFunc";
 
 export default function PostingForm() {
-  const { register, handleSubmit, setValue, getValues } = useForm<IFormData>();
-  const [data, setData] = useState<string>("");
+  const [formattedTextarea, setFormattedTextarea] = useState<boolean>(false);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [alertVariant, setAlertVariant] = useState<string>("");
+  const [alertMessage, setAlertMessage] = useState<string>("");
+
+  const today = new Date();
+  const { register, handleSubmit, reset, setValue, getValues } =
+    useForm<IFormData>({
+      defaultValues: {
+        postingUrl: "",
+        jobTitle: "",
+        company: "",
+        location: "",
+        jobType: ["Full-time"],
+        jobLevel: "Junior",
+        applicationDueDate: today.toISOString().slice(0, 10),
+        responsibilities: "",
+        qualifications: "",
+        skills: "",
+        other: ""
+      }
+    });
+
   const onSubmit: SubmitHandler<IFormData> = (data) => {
-    const dataNormalized: IPostingPost = normalizeData(data);
-    const dataNormalizedJSON = JSON.stringify(dataNormalized);
-    setData(dataNormalizedJSON);
+    const dataNormalized: IPostingPost = normalizeFormData(data);
     axios
       .post(`${baseURL}/api/postings`, dataNormalized)
       .then((response) => {
         console.log(response.data);
+        if (response.status == 201) {
+          reset();
+          setFormattedTextarea(false);
+          setAlertVariant("success");
+          setAlertMessage("Posting added successfully");
+          setShowAlert(true);
+        }
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setAlertVariant("danger");
+        setAlertMessage("Posting added unsuccessfully");
+        setShowAlert(true);
+      });
   };
 
   const formatTextarea = () => {
+    const convertToHTML = (text: string): string => {
+      return (
+        "- " +
+        text.replaceAll(/[-][\s]+/g, "").replace(/(\s*<br>)*[\n]+/g, "\n- ")
+      );
+    };
+
     setValue("responsibilities", convertToHTML(getValues("responsibilities")));
     setValue("qualifications", convertToHTML(getValues("qualifications")));
+    setFormattedTextarea(true);
   };
 
-  const convertToHTML = (text: string): string => {
-    return (
-      "- " +
-      text.replaceAll(/[-][\s]+/g, "").replace(/(\s*<br>)*[\n]+/g, "\n- ")
-    );
-  };
-
-  const normalizeData = (data: IFormData): IPostingPost => {
-    const {
-      postingUrl,
-      jobTitle,
-      company,
-      location,
-      jobType,
-      jobLevel,
-      applicationDueDate,
-      responsibilities,
-      qualifications,
-      skills,
-      other
-    } = data;
-    const dataNormalized: IPostingPost = {
-      postingUrl,
-      jobTitle,
-      company,
-      location,
-      jobType,
-      jobLevel,
-      applicationDueDate,
-      responsibilities: normalizeTextarea(responsibilities),
-      qualifications: normalizeTextarea(qualifications),
-      skills: skills.split(", "),
-      other
-    };
-    return dataNormalized;
-  };
-
-  const normalizeTextarea = (text: string): string[] => {
-    return text
-      .slice(2)
-      .split("\n- ")
-      .filter((entry) => entry !== "");
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+  }, [alertVariant]);
 
   return (
     <form className="mb-3" onSubmit={handleSubmit(onSubmit)}>
+      {showAlert && (
+        <div className={`alert alert-${alertVariant}`}>{alertMessage}</div>
+      )}
       <div className="mb-3">
         <label className="form-label">URL*</label>
         <input
@@ -137,7 +142,7 @@ export default function PostingForm() {
           />
         </div>
       </div>
-      <div>
+      <div className="mb-3">
         <label className="form-label">Responsibilities*</label>
         <textarea
           className="form-control"
@@ -145,7 +150,7 @@ export default function PostingForm() {
           style={{ height: "250px" }}
         />
       </div>
-      <div>
+      <div className="mb-3">
         <label className="form-label">Qualifications*</label>
         <textarea
           className="form-control"
@@ -153,26 +158,46 @@ export default function PostingForm() {
           style={{ height: "250px" }}
         />
       </div>
-      <div>
+      <div className="mb-3">
         <label className="form-label">Skills*</label>
         <input className="form-control" {...register("skills")} />
       </div>
-      <div>
+      <div className="mb-3">
         <label className="form-label">Other</label>
         <input className="form-control" {...register("other")} />
       </div>
 
-      <p>{data}</p>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={formatTextarea}
-      >
-        Format textarea
-      </button>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
+      <div className="row row-cols-auto mb-3">
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={formatTextarea}
+          >
+            Format textarea
+          </button>
+        </div>
+        <div className="col">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={!formattedTextarea}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      <div className="row row-cols-auto">
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
---
+++ src/pages/NewPosting/helperFunc.ts
@@ -0,0 +1,40 @@
+import { IFormData, IPostingPost } from "../../interfaces";
+
+const normalizeFormData = (data: IFormData): IPostingPost => {
+  const normalizeTextarea = (text: string): string[] => {
+    return text
+      .slice(2)
+      .split("\n- ")
+      .filter((entry) => entry !== "");
+  };
+
+  const {
+    postingUrl,
+    jobTitle,
+    company,
+    location,
+    jobType,
+    jobLevel,
+    applicationDueDate,
+    responsibilities,
+    qualifications,
+    skills,
+    other
+  } = data;
+  const dataNormalized: IPostingPost = {
+    postingUrl,
+    jobTitle,
+    company,
+    location,
+    jobType,
+    jobLevel,
+    applicationDueDate,
+    responsibilities: normalizeTextarea(responsibilities),
+    qualifications: normalizeTextarea(qualifications),
+    skills: skills.split(/\s*,\s*/),
+    other
+  };
+  return dataNormalized;
+};
+
+export { normalizeFormData };
