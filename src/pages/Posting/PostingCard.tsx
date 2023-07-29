667031678c2b323dd6e334da6163bc1633d77c31
Alex Yang <alex.hyang.dev@gmail.com>
2023-07-29 16:13:26 -0700

    improve page structure

--- src/pages/Posting/PostingCard.tsx
+++ src/pages/Posting/PostingCard.tsx
@@ -16,32 +16,53 @@ export default function PostingCard(props: { data: IPostingGet }) {
     other
   } = props.data;
   return (
-    <div>
-      <h3>{jobTitle}<span><a target="_blank" rel="noopener noreferrer" href={postingUrl}>link</a></span></h3>
-      <p>Company: {company}</p>
-      <p>Location: {location}</p>
-      <p>Job Level: {jobLevel}</p>
-      <p>Job Type: {jobType}</p>
-      <p>Due Date: {applicationDueDate.slice(0, 10)}</p>
-      <p>Responsibilities</p>
-      <ul>
-        {responsibilities.map((elem, index) => (
-          <li key={index}>{elem}</li>
-        ))}
-      </ul>
-      <p>Qualifications</p>
-      <ul>
-        {qualifications.map((elem, index) => (
-          <li key={index}>{elem}</li>
-        ))}
-      </ul>
-      <p>Skills</p>
-      <ul>
-        {skills.map((elem, index) => (
-          <li key={index}>{elem}</li>
-        ))}
-      </ul>
-      <p>{other}</p>
-    </div>
+    <article>
+      <header>
+        <h2>
+          {jobTitle}{" "}
+          <span>
+            <a target="_blank" rel="noopener noreferrer" href={postingUrl}>
+              link
+            </a>
+          </span>
+        </h2>
+        <p>
+          {company} - {location}
+        </p>
+      </header>
+      <section>
+        <div>
+          <p>Job Level: {jobLevel}</p>
+          <p>Job Type: {jobType}</p>
+          <p>{`Due Date: ${applicationDueDate.slice(0, 10)}`}</p>
+        </div>
+        <div>
+          <h4>Responsibilities</h4>
+          <ul>
+            {responsibilities.map((elem, index) => (
+              <li key={index}>{elem}</li>
+            ))}
+          </ul>
+          <h4>Qualifications</h4>
+          <ul>
+            {qualifications.map((elem, index) => (
+              <li key={index}>{elem}</li>
+            ))}
+          </ul>
+          <h4>Skills</h4>
+          <ul>
+            {skills.map((elem, index) => (
+              <li key={index}>{elem}</li>
+            ))}
+          </ul>
+          {other && (
+            <>
+              <h4>Notes</h4>
+              <p>{other}</p>
+            </>
+          )}
+        </div>
+      </section>
+    </article>
   );
 }
